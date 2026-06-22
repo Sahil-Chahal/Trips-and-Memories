5c9429b393a3708e0a617f886f4ad34415c3fed0
Sahil_Chahal <chahal.sahil0102@gmail.com>
2026-06-22 13:43:54 +0530

    Redeployed

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ import morgan from "morgan";
 const app = express();
 
 app.use(cors({
-    origin : [process.env.FRONTEND_URL],
+    origin : process.env.FRONTEND_URL,
     credentials : true
 }))
 
